06e6c2ab91ee37429432882c7d94d552252601d0
vikash kumar <vikash kumar@LAPTOP-THHT2UM9>
2021-12-30 00:37:25 +0530

    login page created

---
+++ SDET-27/src/test/java/pratice/LoginPage.java
@@ -0,0 +1,12 @@
+package pratice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+	@Test
+	public void Login() {
+		Reporter.log("loginpage sucessfullty created ");
+	}
+
+}
