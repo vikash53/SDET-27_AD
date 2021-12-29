de787833cf0b618f66f6dcfff7074ed2b5d38c28
vikash kumar <vikash kumar@LAPTOP-THHT2UM9>
2021-12-29 17:50:41 +0530

    import

---
+++ SDET-27/src/test/java/com/crm/autodesk/contacttest/DemoImport.java
@@ -0,0 +1,13 @@
+package com.crm.autodesk.contacttest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoImport {
+	@Test
+	public static void creatcontact() {
+		Reporter.log("login Sucessful");
+	}
+	
+
+}
