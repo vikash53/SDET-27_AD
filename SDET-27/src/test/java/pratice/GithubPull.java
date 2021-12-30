c77fde7fdd58736a6d3350dba6eadf69457e66b2
vikash kumar <vikash kumar@LAPTOP-THHT2UM9>
2021-12-30 09:42:26 +0530

    second push

---
+++ SDET-27/src/test/java/pratice/GithubPull.java
@@ -0,0 +1,12 @@
+package pratice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPull {
+	@Test
+	public  void createTyy() {
+	Reporter.log("pull from github");
+}
+
+}
